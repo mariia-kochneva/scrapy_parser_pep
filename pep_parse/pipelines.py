721a00512202f17bc82863c18656b79d240e215b
Mariia Kochneva <mas.vilis092vi@gmail.com>
2026-06-20 17:02:14 +0500

    #4

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -10,28 +10,17 @@ class PepParsePipeline:
 
     def open_spider(self, spider):
         self.status_counts = {}
-        self.pep_items = []
 
     def process_item(self, item, spider):
         status = item.get('status')
         if status:
             self.status_counts[status] = self.status_counts.get(status, 0) + 1
-        self.pep_items.append(item)
         return item
 
     def close_spider(self, spider):
         results_dir = RESULTS_DIR
         results_dir.mkdir(exist_ok=True)
         now = datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
-        existing_pep = list(results_dir.glob('pep_*.csv'))
-        if not existing_pep:
-            pep_filename = results_dir / f'pep_{now}.csv'
-            with open(pep_filename, 'w', encoding='utf-8') as f:
-                f.write('number,name,status\n')
-                for item in self.pep_items:
-                    f.write(
-                        f"{item['number']},{item['name']},{item['status']}\n"
-                    )
         summary_filename = results_dir / f'status_summary_{now}.csv'
         total = sum(self.status_counts.values())
         with open(summary_filename, 'w', encoding='utf-8') as f:
